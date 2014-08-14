18af7c4ddabb1901b39253c4982af40729a8809d
shitou <lemond.wu@outlook.com>
2014-08-15 00:59:50 +0800

    [shitou] - add spring context.xml,add servlet class WechatServlet

---
+++ src/main/java/shitou/wechat/web/WechatServlet.java
@@ -0,0 +1,30 @@
+package shitou.wechat.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created in Intellij IDEA 13 Ultimate
+ *
+ * @Author shitou
+ * On 2014/8/14 23:40
+ */
+public class WechatServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String signature = req.getParameter("signature");
+        String timestamp = req.getParameter("timestamp");
+        String nonce = req.getParameter("nonce");
+        String echostr = req.getParameter("echostr");
+
+    }
+}

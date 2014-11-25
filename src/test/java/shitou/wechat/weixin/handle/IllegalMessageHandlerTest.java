6f00195d4973c3d7832daec001798a75cfb6c88e
shitou <lemond.wu@outlook.com>
2014-11-26 01:06:44 +0800

    [shitou] - move illegal message handle method to IllegalMessageHandler

---
+++ src/test/java/shitou/wechat/weixin/handle/IllegalMessageHandlerTest.java
@@ -0,0 +1,45 @@
+package shitou.wechat.weixin.handle;
+
+import org.junit.Test;
+import junit.framework.TestCase;
+
+import java.util.List;
+
+
+/**
+ * Created in Intellij IDEA 13 Ultimate
+ * User: shitou
+ * Date: 2014/11/26
+ * Time: 01:03
+ */
+public class IllegalMessageHandlerTest extends TestCase {
+
+    @Test
+    public void testSplitTextMsgTo3Parts() throws Exception {
+        String xml = "<xml>" +
+                "<ToUserName><![CDATA[toUser]]></ToUserName>" +
+                "<FromUserName><![CDATA[fromUser]]></FromUserName>" +
+                "<CreateTime>1348831860</CreateTime>" +
+                "<MsgType><![CDATA[text]]></MsgType>" +
+                "<Content><![CDATA[<Content><![CDATA[this is a illegal text message xml]]></Content>]]></Content>" +
+                "<MsgId>1234567890123456</MsgId>" +
+                "</xml>";
+
+        List<String> splitedMsg = IllegalMessageHandler.splitTextMsgTo3Parts(xml);
+        String expectedXmlPart1 = "<xml><ToUserName><![CDATA[toUser]]></ToUserName><FromUserName><![CDATA[fromUser]]></FromUserName><CreateTime>1348831860</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[";
+
+        assertNotNull(splitedMsg);
+        assertEquals(expectedXmlPart1, splitedMsg.get(0));
+        assertEquals("<Content><![CDATA[this is a illegal text message xml]]></Content>", splitedMsg.get(1));
+        assertEquals("]]></Content><MsgId>1234567890123456</MsgId></xml>", splitedMsg.get(2));
+    }
+
+    @Test
+    public void testIfTextMsgContentIllegal() throws Exception {
+        String content = "<Content><![CDATA[this is a illegal text message xml]]></Content>";
+
+        boolean ifIllegal = IllegalMessageHandler.ifTextMsgContentIllegal(content);
+
+        assertTrue(ifIllegal);
+    }
+}
--- src/test/java/shitou/wechat/weixin/util/WechatUtilsTest.java
+++ src/test/java/shitou/wechat/weixin/util/WechatUtilsTest.java
@@ -71,33 +71,4 @@ public class WechatUtilsTest extends TestCase {
         assertEquals("user_fdkaucskfooshuf", textMessageModel.getToUserName());
         assertEquals("me_iefhfe83r4eawfue", textMessageModel.getFromUserName());
     }
-
-    @Test
-    public void testSplitTextMsgTo3Parts() throws Exception {
-        String xml = "<xml>" +
-                "<ToUserName><![CDATA[toUser]]></ToUserName>" +
-                "<FromUserName><![CDATA[fromUser]]></FromUserName>" +
-                "<CreateTime>1348831860</CreateTime>" +
-                "<MsgType><![CDATA[text]]></MsgType>" +
-                "<Content><![CDATA[<Content><![CDATA[this is a illegal text message xml]]></Content>]]></Content>" +
-                "<MsgId>1234567890123456</MsgId>" +
-                "</xml>";
-
-        List<String> splitedMsg = WechatUtils.splitTextMsgTo3Parts(xml);
-        String expectedXmlPart1 = "<xml><ToUserName><![CDATA[toUser]]></ToUserName><FromUserName><![CDATA[fromUser]]></FromUserName><CreateTime>1348831860</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[";
-
-        assertNotNull(splitedMsg);
-        assertEquals(expectedXmlPart1, splitedMsg.get(0));
-        assertEquals("<Content><![CDATA[this is a illegal text message xml]]></Content>", splitedMsg.get(1));
-        assertEquals("]]></Content><MsgId>1234567890123456</MsgId></xml>", splitedMsg.get(2));
-    }
-
-    @Test
-    public void testIfTextMsgContentIllegal() throws Exception {
-        String content = "<Content><![CDATA[this is a illegal text message xml]]></Content>";
-
-        boolean ifIllegal = WechatUtils.ifTextMsgContentIllegal(content);
-
-        assertTrue(ifIllegal);
-    }
 }
